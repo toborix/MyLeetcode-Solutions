814464b40b49a257667d40ee83a5aa7e6f8252df
toborix <matveybor394@gmail.com>
2022-11-30 20:55:21 +0300

    Add a solution of "Longest Substring"

---
+++ src/Levels/Medium/LongestSubstrWtRepeatingCharacters/Solution_1.java
@@ -0,0 +1,38 @@
+package Levels.Medium.LongestSubstrWtRepeatingCharacters;
+
+/**
+ * Используем Вложенные - это O(n^2)
+ * Алгоритм Слишком медленный
+ */
+
+public class Solution_1 {
+    public int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder currentSubstring = new StringBuilder();
+            for (int j = i; j < s.length(); j++) {
+                if(currentSubstring.indexOf(String.valueOf(s.charAt(j))) != -1){
+                    break;
+                }
+                currentSubstring.append(s.charAt(j));
+                maxLength = Math.max(maxLength, currentSubstring.length());
+
+                /*
+                кусок код ниже вопляет те же действия,
+                что строчка ниже
+                 */
+    //                if(currentSubstring.length() > maxLength){
+    //                    maxLength = currentSubstring.length();
+    //                }
+
+            }
+
+        }
+        return maxLength;
+
+    }
+
+}
+
+
---
+++ src/Levels/Medium/LongestSubstrWtRepeatingCharacters/Solution_3.java
@@ -0,0 +1,22 @@
+package Levels.Medium.LongestSubstrWtRepeatingCharacters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_3 {
+    public int lengthOfLongestSubstring(String s){
+        int maxLength = 0;
+
+        for (int right = 0, left = 0; right < s.length(); right++) {
+           int indexlocationOfFirstApperanceInSubstring = s.indexOf(s.charAt(right), left);
+           if  (indexlocationOfFirstApperanceInSubstring != right){
+               left = indexlocationOfFirstApperanceInSubstring + 1;
+           }
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+
+        return maxLength;
+    }
+
+}
