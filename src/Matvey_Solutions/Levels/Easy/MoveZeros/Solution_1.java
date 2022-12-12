9a1414743b628653878b3a7074490cd83f80d441
toborix <matveybor394@gmail.com>
2022-12-12 15:04:32 +0300

    Add a solution of "Move Zeros"

---
+++ src/Matvey_Solutions/Levels/Easy/MoveZeros/Solution_1.java
@@ -0,0 +1,16 @@
+package Matvey_Solutions.Levels.Easy.MoveZeros;
+
+public class Solution_1 {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0){
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        for(int i = lastNonZeroFoundAt; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
