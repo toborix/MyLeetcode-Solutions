d0aa36b66c18d1715ceb45b2c7c4028b1d9aeef8
toborix <matveybor394@gmail.com>
2023-03-03 12:30:12 +0300

    Add a solution of "350"

---
+++ src/Matvey_Solutions/Levels/Easy/IntersectionofTwoArraysII_350/Solution_1.java
@@ -0,0 +1,29 @@
+package Matvey_Solutions.Levels.Easy.IntersectionofTwoArraysII_350;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution_1 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        HashMap<Integer, Integer> m = new HashMap<>();
+        /*
+            Метод getOrDefault() - метод getOrDefault(),
+             позволяющий задать значение по умолчанию
+         */
+        for (int n : nums1) {
+            m.put(n, m.getOrDefault(n, 0) + 1);
+        }
+        int k = 0;
+        for (int n : nums2) {
+            int cnt = m.getOrDefault(n, 0);
+            if (cnt > 0) {
+                nums1[k++] = n;
+                m.put(n, cnt - 1);
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+}
